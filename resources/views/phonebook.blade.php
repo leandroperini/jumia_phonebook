5e8838109b4debced5870983e8cf62bda7ce2f50
Leandro Perini <leandroperini@users.noreply.github.com>
2021-07-06 23:16:38 -0300

    basic phonebook page

--- resources/views/phonebook.blade.php
+++ resources/views/phonebook.blade.php
@@ -1,15 +1,85 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Jumia Phobebook</title>
+    <title>Jumia Phobebook</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    </head>
-    <body class="antialiased">
+</head>
+<body class="antialiased">
+<div class="row justify-content-start">
+    <div class="col">
+        <h1>Phone numbers</h1>
+    </div>
+</div>
+
+<form action="">
+    <div class="row justify-content-start">
+        <div class="col-4">
+            <select id="country_select" name="country_select" class="form-select" aria-label="Select a country">
+                <option selected>Select a Country</option>
+                <option value="1">Option one</option>
+                <option value="2">Option two</option>
+            </select>
+        </div>
+        <div class="col-4">
+            <select id="state_select" name="state_select" class="form-select" aria-label="Select a validity">
+                <option selected>Select a validity state</option>
+                <option value="1">Valid</option>
+                <option value="2">Invalid</option>
+            </select>
+        </div>
+    </div>
+</form>
+<div class="row justify-content-start">
+    <div class="col">
+        <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+<div class="row justify-content-md-center">
+    <div class="col">
         <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
             Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
         </div>
-    </body>
+    </div>
+</div>
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+
+</body>
 </html>
